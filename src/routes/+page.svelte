efd3e43e83fccf394da80a615cf0089e3bbe5172
aeither <36173828+aeither@users.noreply.github.com>
2022-09-15 19:02:06 +0200

    realtime

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,33 +3,37 @@
 	import Input from '$lib/Input.svelte';
 	import { onMount } from 'svelte';
 
-	let txs = '';
+	let txs = {
+		values: ['']
+	};
 
-	onMount(async () => {
-		const res = await fetch('/api/random-number?min=20&max=100');
-		const data = await res.json();
-		txs = data;
-		console.log('🚀 ~ file: +page.svelte ~ line 12 ~ onMount ~ data', data);
+	onMount(() => {
+		async function fetchData() {
+			const res = await fetch('/api/random-number?min=20&max=100');
+			const data = await res.json();
+			txs = data;
+		}
+
+		const interval = setInterval(fetchData, 3000);
+		fetchData();
+
+		return () => clearInterval(interval);
 	});
-
-	async function handleLogin(e: any) {
-		e.preventDefault();
-	}
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center text-center">
 	<div>
 		<h1>Welcome to SvelteKit</h1>
-		<div>
-			<span>Hash: {txs}</span>
-		</div>
+		{#each txs.values as value}
+			<div>
+				<span>Hash: {value}</span>
+			</div>
+		{/each}
 		<Input />
 		<InputSlider value="slider" />
-		<form on:submit={handleLogin} class="card card-body" style="max-width: 400px">
-			<button type="submit" class="btn btn-primary">Log In</button>
-		</form>
 
-		<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+		<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Say Hi!</button
+		>
 	</div>
 </div>
 
--- src/routes/api/random-number/+server.ts
+++ src/routes/api/random-number/+server.ts
@@ -2,38 +2,45 @@ import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 import { error } from '@sveltejs/kit';
 import { readFileSync } from 'fs';
 
-/** @type {import('./$types').RequestHandler} */
-export function GET({ url }: { url: URL }) {
-	// Deployer
-	// const CHAIN_ID = 'mumbai';
-	// const DEPLOYER_ADDRESS = '0x7c487845f98938Bb955B1D5AD069d9a30e4131fd';
-	// const ABI_JSON = readFileSync('src/data/deployerAbi.json', 'utf-8');
+const values = ['hello', 'world', 'time'];
+setInterval(() => values.push('hello'), 3000);
 
-	// USDC
-	const CHAIN_ID = 'polygon';
-	const DEPLOYER_ADDRESS = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
-	const ABI_JSON = readFileSync('src/data/USDC.json', 'utf-8');
+// Deployer
+// const CHAIN_ID = 'mumbai';
+// const DEPLOYER_ADDRESS = '0x7c487845f98938Bb955B1D5AD069d9a30e4131fd';
+// const ABI_JSON = readFileSync('src/data/deployerAbi.json', 'utf-8');
 
-	const sdk = new ThirdwebSDK(CHAIN_ID);
-	const contract = sdk.getContractFromAbi(DEPLOYER_ADDRESS, JSON.parse(ABI_JSON));
+// USDC
+const CHAIN_ID = 'polygon';
+const DEPLOYER_ADDRESS = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+const ABI_JSON = readFileSync('src/data/USDC.json', 'utf-8');
 
-	contract.events.listenToAllEvents((event) => {
-		if (event.eventName === 'Approval') {
-			console.log(event);
-			return new Response(event.transaction.transactionHash);
+const sdk = new ThirdwebSDK(CHAIN_ID);
+const contract = sdk.getContractFromAbi(DEPLOYER_ADDRESS, JSON.parse(ABI_JSON));
+
+// poll
+const txHashes = [''];
+contract.events.listenToAllEvents((event) => {
+	if (event.eventName === 'Transfer') {
+		if (txHashes.length > 30) {
+			txHashes.splice(0, txHashes.length - 25);
+			txHashes.push(event.transaction.transactionHash);
+		} else {
+			txHashes.push(event.transaction.transactionHash);
 		}
-	});
-
-	const min = Number(url.searchParams.get('min') ?? '0');
-	const max = Number(url.searchParams.get('max') ?? '1');
-
-	const d = max - min;
-
-	if (isNaN(d) || d < 0) {
-		throw error(400, 'min and max must be numbers, and min must be less than max');
+		// console.log('txHashes', txHashes);
 	}
+});
 
-	const random = min + Math.random() * d;
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }: { url: URL }) {
+	if (url) {
+		const min = Number(url.searchParams.get('min') ?? '0');
+	}
+	// if (false) {
+	// 	throw error(400, 'min and max must be numbers, and min must be less than max');
+	// }
 
-	return new Response(String(random));
+	const myOptions = { status: 200, statusText: 'Great!' };
+	return new Response(JSON.stringify({ values: txHashes }), myOptions);
 }
