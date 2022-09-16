7fc1d718bc579b94a3cea5d44b305beac710c6e9
aeither <36173828+aeither@users.noreply.github.com>
2022-09-16 17:37:55 +0200

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.postcss';
 </script>
 
+<div class="navbar bg-base-100">
+	<a href="/" class="btn btn-ghost text-xl normal-case">Mumbai Testnet</a>
+	<a href="/polygon" class="btn btn-ghost text-xl normal-case">Polygon Mainnet</a>
+</div>
 <slot />
